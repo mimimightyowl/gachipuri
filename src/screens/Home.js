c5471e93401fc0abeb1bff2f6baa111c33884751
Tracy <rudi.sergeev@gmail.com>
2022-06-26 01:22:26 +0400

    feat: config and test setup

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 import { defaultSafeAreaEdges } from '../common/config';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { Button, Divider, Layout, TopNavigation } from '@ui-kitten/components';
+import { Button, Layout, Text } from '@ui-kitten/components';
+import { View } from 'react-native';
 
 export const HomeScreen = ({ navigation }) => {
   const navigateDetails = () => {
     navigation.navigate('Details');
   };
 
+  const getTodos = async () => {
+    return fetch('https://jsonplaceholder.typicode.com/todos?userId=5')
+      .then(response => response.json())
+      .catch(error => console.log(error));
+  };
+
+  const query = useQuery('todos', getTodos);
+
   return (
     <SafeAreaView style={{ flex: 1 }} edges={defaultSafeAreaEdges}>
-      <TopNavigation title="MyApp" alignment="center" />
-      <Divider />
       <Layout
         style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <View>
+          {query.data.map(todo => (
+            <Text key={todo.id}>{todo.title}</Text>
+          ))}
+        </View>
         <Button onPress={navigateDetails}>OPEN DETAILS</Button>
       </Layout>
     </SafeAreaView>
